98c775ec0cd01d34d947cbd344c44f2a49830417
StevenDanesswaralay <55594713+StevenDanesswaralay@users.noreply.github.com>
2019-09-26 11:18:21 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/pbo2/latihan6/kambingstatic/Mamalia.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo2.latihan6.kambingstatic;
+
+/**
+ *
+ * @author 
+ * NAMA     : Steven Danesswaralay
+ * KELAS    : PBO2
+ * NIM      : 10118078
+ * Deskripsi Program : Mamalia
+ */
+public class Mamalia {
+    //    variabel jumlahKambing dideklarasikan sebagai statik
+    public static int jumlahKambing; 
+}
